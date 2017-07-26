a03686a258a6bef44de0e4235dfc26b945e6d96e
Ben Winding <ben.winding@gmail.com>
2017-07-27 00:11:23 +0930

    Made menu taller, corrected css central alignment issues

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import MyMenuRoutes from "./MyMenu/MyMenuRoutes";
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <div className="App" id="outer-container">
         <MyMenu />
-        <MyMenuRoutes />
+        <MyMenuRoutes id="page-wrap"/>
       </div>
     );
   }
--- src/SignInPage/SignInPage.js
+++ src/SignInPage/SignInPage.js
@@ -16,16 +16,16 @@ export default class SignInPage extends Component {
           <div className="video-foreground">
             <iframe
               src={youTubeString}
-              frameborder="0"
-              allowfullscreen
+              allowFullScreen
+              frameBorder={0}
             />
           </div>
         </div>
 
         <div id="vidtop-content">
           <div className="vid-info">
-            <div style={{"padding-bottom":10}}>
-              <img src={logo} className="App-logo" alt="logo" />
+            <div className="Signin-logo">
+              <img src={logo} alt="logo" />
             </div>
             <GoogleLogin
               clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
