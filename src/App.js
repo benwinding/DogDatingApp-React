daba950af5813187f6b92fc9f52f2285f248db91
Ben Winding <ben.winding@gmail.com>
2017-07-26 21:54:39 +0930

    Video added!

--- src/App.js
+++ src/App.js
@@ -9,18 +9,32 @@ const responseGoogle = (response) => {
 
 export default class App extends Component {
   render() {
-
+    // let clipId = "IGJCx2ug0lU";
+    let youTubeString = "https://www.youtube.com/embed/IGJCx2ug0lU?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=IGJCx2ug0lU";
     return (
-      <div className="Login-screen">
-        <Center
-          style={{width:"100vw", height:"100vh"}}>
-          <GoogleLogin
-            clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
-            buttonText="Google"
-            onSuccess={responseGoogle}
-            onFailure={responseGoogle}
-          />
-        </Center>
+      <div>
+        <div className="video-background">
+          <div className="video-foreground">
+            <iframe
+              src={youTubeString}
+              frameborder="0"
+              allowfullscreen
+            ></iframe>
+          </div>
+        </div>
+
+        <div id="vidtop-content">
+          <div className="vid-info">
+            <div className="Login-screen">
+              <GoogleLogin
+                clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+                buttonText="Google"
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
