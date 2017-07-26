535c601bf50a59a86902172e8ba50fce24ee22f0
Ben Winding <ben.winding@gmail.com>
2017-07-26 22:07:52 +0930

    Font changed

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ const responseGoogle = (response) => {
 
 export default class App extends Component {
   render() {
-    // let clipId = "IGJCx2ug0lU";
     let youTubeString = "https://www.youtube.com/embed/IGJCx2ug0lU?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=IGJCx2ug0lU";
     return (
       <div>
@@ -25,17 +24,15 @@ export default class App extends Component {
 
         <div id="vidtop-content">
           <div className="vid-info">
-            <div className="Login-screen">
-              <div style={{"padding-bottom":10}}>
-                <img src={logo} className="App-logo" alt="logo" />
-              </div>
-              <GoogleLogin
-                clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
-                buttonText="Google Sign In"
-                onSuccess={responseGoogle}
-                onFailure={responseGoogle}
-              />
+            <div style={{"padding-bottom":10}}>
+              <img src={logo} className="App-logo" alt="logo" />
             </div>
+            <GoogleLogin
+              clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+              buttonText="Google Sign In"
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+            />
           </div>
         </div>
       </div>
